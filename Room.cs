d2e318f01b2735ebe70c86b2b8cc919ddbb14c85
Increase27 <alexandersj2018@gmail.com>
2025-03-13 13:43:31 +0000

    Updated Room.cs

--- Room.cs
+++ Room.cs
@@ -2,23 +2,31 @@
 {
     public class Room
     {
+
         private string Description;
         public string Item;
-        
-        public Room(string Description, string Item)
+        public int DoorLocked;
+
+        public Room(string Description, string Item, int DoorLocked)
         {
             this.Description = Description;
             this.Item = Item;
+            this.DoorLocked = DoorLocked;
         }
 
         public string GetDescription() // Returns the rooms description
         {
             return Description;
         }
-    
+        
         public string GetItem() // Returns the rooms item
         {
             return Item;
+        }
+        
+        public int CheckDoorLocked() 
+        {
+            return DoorLocked;
         }
     }
 }
